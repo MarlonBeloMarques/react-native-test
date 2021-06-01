7a3fefba00b1858947dcf776fc9a7623c4b12d99
MarlonBeloMarques <marlon.belohd@gmail.com>
2021-06-01 18:03:18 -0400

    configuracao de teste com jest e react-test-renderer

--- App.js
+++ App.js
@@ -5,17 +5,29 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.welcome}>Welcome to React Native!</Text>
+      <Text style={styles.instructions}>
+        This is a React Native snapshot test.
+      </Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    alignItems: "center",
+    backgroundColor: "#F5FCFF",
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+  },
+  instructions: {
+    color: "#333333",
+    marginBottom: 5,
+    textAlign: "center",
+  },
+  welcome: {
+    fontSize: 20,
+    margin: 10,
+    textAlign: "center",
   },
 });
