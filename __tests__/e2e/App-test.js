ed2997cdef19b2b3b2c89fd207e5b1f8f8e5cf2a
MarlonBeloMarques <marlon.belohd@gmail.com>
2021-06-05 20:55:52 -0400

    testa se possui elemento texto e o texto boas vindas

--- __tests__/e2e/App-test.js
+++ __tests__/e2e/App-test.js
@@ -1,25 +1,40 @@
 import React from "react";
-import { Text } from "react-native";
+import { Text, View } from "react-native";
 import renderer from "react-test-renderer";
-// import { ActivityIndicator } from "react-native";
-//import TeamsList from "../../src/components/TeamsList";
+import { ActivityIndicator } from "react-native";
+import TeamsList from "../../src/components/TeamsList";
 import App from "../../App";
 
-// test("renders correctly", () => {
-//   const tree = renderer.create(<App />).toJSON();
-//   expect(tree).toBeTruthy();
-// });
+test("renders correctly", () => {
+  const tree = renderer.create(<App />).toJSON();
+  expect(tree).toBeTruthy();
+});
 
-// it("renders the loading component when the app is first rendered", () => {
-//   const rendered = renderer.create(<App />);
-//   const TeamsListRendered = rendered.root.findByType(TeamsList);
-//   const ActivityIndicatorRendered =
-//     TeamsListRendered.findByType(ActivityIndicator);
-//   expect(ActivityIndicatorRendered).toBeTruthy();
-// });
+it("renders the loading component when the app is first rendered", () => {
+  const rendered = renderer.create(<App />);
+  const TeamsListRendered = rendered.root.findByType(TeamsList);
+  const ActivityIndicatorRendered =
+    TeamsListRendered.findByType(ActivityIndicator);
+  expect(ActivityIndicatorRendered).toBeTruthy();
+});
+
+it("renders welcome to react native", () => {
+  const rendered = renderer.create(<App />);
+  const root = rendered.root;
+  expect(root.findAllByType(Text)[0].props.children).toEqual('Welcome to React Native!');
+});
+
+it("renders text", () => {
+  const rendered = renderer.create(<App/>);
+  const root = rendered.root;
+  expect(root.findByType(View).findAllByType(Text)).toBeTruthy();
+
+})
 
 it("renders welcome", () => {
   const rendered = renderer.create(<App />);
   const root = rendered.root;
-  expect(root.findAllByType(Text)[0].props.children).toEqual('Welcome to React Native!');
+  expect(root.findAllByType(Text)[0].props.children.includes("Welcome")).toBe(
+    true
+  );
 });
